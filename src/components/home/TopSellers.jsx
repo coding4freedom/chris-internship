dfac72f5d51d45acaeffead00c195e0df8d90000
coding4freedom <chawkins2010@hotmail.com>
2025-02-23 15:13:20 -0600

    added Skeleton for data loading transition

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const TopSellers = () => {
   const [ topData, setTopData ] = useState([]);
+  const [ loading, setLoading ] = useState(false);
 
   useEffect(() => {
     async function fetchTopSeller()  {
-      try {      
+      try {
+        setLoading(true)      
         const response = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers");
-        setTopData(response.data);              
+        setTopData(response.data);
+        setLoading(false)              
       } catch (error) {
         console.error('Error while loading data', error)
       }
@@ -29,7 +34,13 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {topData.map((seller) => (
+              {loading
+              ? Array.from({length: 12}).map((_, index) => (
+                <li key={index}>
+                <SkeletonSellers />
+                </li>
+              ))
+              :topData.map((seller) => (
                 <li key={seller.id}>
                   <div className="author_list_pp">
                     <Link to={`/author/${seller.authorId}`}>
@@ -54,5 +65,18 @@ const TopSellers = () => {
     </section>
   );
 };
+
+const SkeletonSellers = () => (
+  <>
+    <div className="author_list_pp">
+      <Skeleton circle width={50} height={50} />
+      <i className="fa fa-check"></i>      
+    </div>
+    <div className="author_list_info">
+      <Skeleton width={150} height={20} />
+      <Skeleton width={50} height={15} />
+    </div>
+  </>
+)
 
 export default TopSellers;
